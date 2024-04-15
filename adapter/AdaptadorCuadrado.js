6f187713902aaa749b344ab61da3dcce9a3582e4
LautaroGVega <141835855+LautaroGVega@users.noreply.github.com>
2024-04-15 17:03:54 -0300

    Update AdaptadorCuadrado.js

--- adapter/AdaptadorCuadrado.js
+++ adapter/AdaptadorCuadrado.js
@@ -1,15 +1,10 @@
 const Cuadrado = require('./Cuadrado');
 const Circulo = require('./Circulo');
 
-class AdaptadorDeCuadrado extends Cuadrado {
-  constructor(forma) {
-    if (forma instanceof Cuadrado) {
-      super(forma.lado);
-    } else if (forma instanceof Circulo) {
-      super(forma.radio * Math.sqrt(2));
-    } else {
-      throw new Error('Forma inválida proporcionada.');
-    }
+class AdaptadorCuadrado extends Cuadrado {
+  constructor(circulo) {
+    const lado = circulo.radio * Math.sqrt(2);
+    super(lado);
   }
 }
 
